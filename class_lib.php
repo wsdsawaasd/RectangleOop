d1f2b89665b01b01de3afb4774b8ac0dce9d04fc
Phat <phatt6898@gmail.com>
2019-04-08 10:33:04 +0700

    add file

---
+++ class_lib.php
@@ -0,0 +1,10 @@
+<?php
+    include "Rectangle.php";
+$rectangle = new Rectangle(100, 200);
+echo  $rectangle->getArea();
+echo "<br/>";
+echo  $rectangle->getPerimeter();
+echo "<br/>";
+echo  $rectangle->Display();
+
+
